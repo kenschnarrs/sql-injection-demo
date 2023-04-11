eafc0ee0838f1ad041d41096118360388f2cc293
kenschnarrs <knschnarrs@gmail.com>
2023-04-11 02:58:48 -0500

    saving work

--- backend/Controllers/MyController.cs
+++ backend/Controllers/MyController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace backend.Controllers;
 
@@ -17,6 +19,40 @@ public class MyController : ControllerBase
     [HttpPost(Name = "ReceiveData")]
     public void Post()
     {
+        try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "https://localhost:3307"; 
+                builder.UserID = "root";            
+                builder.Password = "root";     
+                builder.InitialCatalog = "MySQL";
+         
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+                    
+                    connection.Open();       
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                   /* using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }           */         
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         Console.WriteLine("f");
         //Debug.WriteLine("f2");
     }
